62ad33c82150cf245f5240148bfef5a96a5b66cc
nullt3ch <45492571+nullt3ch@users.noreply.github.com>
2019-07-22 17:00:39 -0500

    Add files via upload

---
+++ Anagram/Anagram/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+
+namespace Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creación de variables 
+            String cadena1, cadena2;
+
+            // Prompt para obtener Inputs 
+            Console.WriteLine("Input de la cadena 1: ");
+            cadena1 = Console.ReadLine();
+            Console.WriteLine("Input de la cadena 2:");
+            cadena2 = Console.ReadLine();
+
+            // Llamada a la función que compara una cadena con la otra para saber si es un anagrama.
+            Boolean resultado = Comparar(cadena1, cadena2);
+            
+            // Resultado en tipo booleano que dice si fue un anagrama o no.
+            if (resultado)
+            {
+                Console.WriteLine("Sí es un anagrama");
+            }
+            else
+            {
+                Console.WriteLine("No es un anagrama");
+            }
+        }
+        public static Boolean Comparar(String cadena1, String cadena2)
+        {
+            // Creación de arreglos de lista para introducir todos los chars de la cadena inputada anteriormente.
+            ArrayList arrayList1 = new ArrayList();
+            ArrayList arrayList2 = new ArrayList();
+
+            // Si no tienen el mismo tamaño las cadenas,automáticamente se elimina la posibilidad de ser un anagrama y se manda el valor de falso.
+            if (cadena1.Length != cadena2.Length)
+                return false;
+
+            // Foreach que pasa los carácteres a los arreglos de lista de las cadenas inputadas. 
+            foreach (char c in cadena1)
+            {
+                arrayList1.Add(c);
+            }
+            foreach (char c in cadena2)
+            {
+                arrayList2.Add(c);
+            }
+
+            // Esta fue una función que encontré con la libreria de Collections, en caso de no tener esta librería,
+            // simplemente se hace el método de la burbuja para obtener el acomodo de los datos 
+            arrayList1.Sort();
+            arrayList2.Sort();
+
+            // For para pasar los arreglos y se comparan cada carácter uno por uno para validar si son iguales, ya que ya están acomodados en la función anterior
+            for (int i = 0; i < arrayList1.Count; i++)
+            {
+                // Se convierten a String porque sin ello no los interpreta de manera correcta. 
+                if (arrayList1[i].ToString() != arrayList2[i].ToString())
+                {
+                    return false ;
+                }
+            }
+            return true;
+            
+        }
+    }
+}
---
+++ ConvertRoman/ConvertRoman/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConvertRoman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Definir las variables de entrada
+            String entero, romano;
+            int converted;
+
+            // Input del usuario y conversión a entero
+            Console.WriteLine("Ingresa un número menor a 3999: ");
+            entero = Console.ReadLine();
+            converted = Convert.ToInt32(entero);
+
+            // Validar si el valor es menor a 4000
+            if (converted < 4000)
+            {
+                // Funcion para convertir a números romanos
+                romano = ConvertRoman(converted);
+                Console.WriteLine("Valor en Romano: " + romano);
+            }
+            // No aceptar valores mayores de 3999
+            else
+            {
+                Console.WriteLine("El valor no puede ser mayor a 3999");
+            }
+        }
+        public static String ConvertRoman(int number)
+        {
+            // Creación de las variables tipo arreglo para definir las letras romanas y su valor 
+            // ** También se puede hacer con un diccionario
+            int[] numerosEnteros = new int[]
+            {
+                1,4,5,9,10,40,50,100,400,500,900,1000
+            };
+            String[] letrasRomanas = new String[]
+            {
+                "i", "iv", "v", "ix", "x", "xl", "l","xc","c","cd","d","cm","m"
+            };
+            string resultado = "";
+
+            // For para repasar los arreglos y definir qué valor tienen 
+            for (int i = 0; i < letrasRomanas.Length; i++)
+            {
+                // Si el número es 0, deja de hacer la iteración 
+                if (number != 0)
+                {
+                    // Aquí está la lógica para de la función. Se valida si el número input es mayor al número actual del arreglo
+                    while (number >= numerosEnteros[i])
+                    {
+                        // Se va eliminando al número inputado hasta llegar a 0 mientras el número sea mayor o igual al del arreglo
+                        number -= numerosEnteros[i];
+                        // Aquí se define cuál número romano tiene y se va añadiendo letra por letra con cada iteración
+                        resultado += letrasRomanas[i];
+                    }
+                }
+            }
+            return resultado;
+        }
+    }
+}
